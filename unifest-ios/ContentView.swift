487ca6df8facd4f43d4275c3c146d9c5a4be22cf
Hoeun Lee <hoeunlee228@gmail.com>
2024-03-13 17:40:32 +0900

    Initial Commit

---
+++ unifest-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  unifest-ios
+//
+//  Created by Hoeun Lee on 3/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ unifest-ios/unifest_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  unifest_iosApp.swift
+//  unifest-ios
+//
+//  Created by Hoeun Lee on 3/13/24.
+//
+
+import SwiftUI
+
+@main
+struct unifest_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
