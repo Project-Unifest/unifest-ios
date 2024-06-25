f0505585a895bc145d4c0fee64c13454a833f96f
Hoeun Lee <hoeunlee228@gmail.com>
2024-06-25 16:02:42 +0900

    [Setting] #12 - 프로젝트 v2 생성

---
+++ unifest-ios2/unifest_ios2App.swift
@@ -0,0 +1,17 @@
+//
+//  unifest_ios2App.swift
+//  unifest-ios2
+//
+//  Created by Hoeun Lee on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct unifest_ios2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
