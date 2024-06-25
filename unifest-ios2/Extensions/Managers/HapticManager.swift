9a224c5b0ea54b170a23224da1d12e27502b121c
Hoeun Lee <hoeunlee228@gmail.com>
2024-06-25 16:23:37 +0900

    [Feat] #14 - HapticManager 구현

---
+++ unifest-ios2/Extensions/Managers/HapticManager.swift
@@ -0,0 +1,24 @@
+//
+//  HapticManager.swift
+//  unifest-ios2
+//
+//  Created by Hoeun Lee on 6/25/24.
+//
+
+import UIKit
+
+final class HapticManager {
+    static let shared = HapticManager()
+    
+    // warning, error, success
+    func hapticNotification(type: UINotificationFeedbackGenerator.FeedbackType) {
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(type)
+    }
+    
+    // heavy, light, meduium, rigid, soft
+    func hapticImpact(style: UIImpactFeedbackGenerator.FeedbackStyle) {
+        let generator = UIImpactFeedbackGenerator(style: style)
+        generator.impactOccurred()
+    }
+}
