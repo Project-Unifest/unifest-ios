6a8ae6a0a24be182ad9d927f5be8b5e469a32c0e
jstar_00 <wltjd6300@naver.com>
2024-10-21 19:37:00 +0900

    [Fix] 일부 디바이스에서 CelendarView의 캘린더 연도, 월 텍스트가 잘리는 문제 해결 / DeviceUUIDManager파일에 TAKUUID copyright 주석 추가

--- unifest-ios/Text/Extensions/Managers/DeviceUUIDManager.swift
+++ unifest-ios/Text/Extensions/Managers/DeviceUUIDManager.swift
@@ -4,6 +4,30 @@
 //
 //  Created by 임지성 on 10/17/24.
 //
+//
+//  This file is part of the unifest-ios project.
+//
+//  This project uses TAKUUID, an open-source library, to manage device UUIDs.
+//  TAKUUID Library:
+//  Copyright (c) 2013-2014 Katsumi Takahashi (https://github.com/taka0125/TAKUUID)
+//
+//  Licensed under the MIT License (MIT). You may use this library in accordance with the MIT License.
+//  The full license is available at: https://opensource.org/licenses/MIT
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
 
 import Foundation
 import TAKUUID
